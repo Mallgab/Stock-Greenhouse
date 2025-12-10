0fdac48ffd9ac47735578694ce44af6ac158e792
Alberts Gusts <mallgab@Albertss-MacBook-Air-2.local>
2025-12-10 12:39:23 +0200

    Configure Vite base for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Stock-Greenhouse/', // Important for deploying to a subpath like GitHub Pages
   server: {
     hmr: { overlay: false },
   },
